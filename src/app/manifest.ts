9ad2364102d521c8080c731081987238900003c5
André Ezequiel <andrelucasmartins@hotmail.com>
2024-05-02 22:33:34 -0300

    feat: PWA and updating components

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,7 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'SB2 Turbo App',
     short_name: 'SB2 Turbo App',
-    description: 'SB2 Turbo App',
+    description: 'SB2 Turbo App - Conheça o suplemento que já ajudou mais de 82 mil pessoas de todas as idades a recuperarem a autoestima e saúde com apenas duas cápsulas ao dia!',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
@@ -15,6 +15,42 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: 'any',
         type: 'image/x-icon',
       },
+      {
+   src: "/android-icon-36x36.png",
+   sizes: "36x36",
+   type: "image/png",
+
+  },
+  {
+   src: "/android-icon-48x48.png",
+   sizes: "48x48",
+   type: "image/png",
+
+  },
+  {
+   src: "/android-icon-72x72.png",
+   sizes: "72x72",
+   type: "image/png",
+
+  },
+  {
+   src: "/android-icon-96x96.png",
+   sizes: "96x96",
+   type: "image/png",
+
+  },
+  {
+   src: "/android-icon-144x144.png",
+   sizes: "144x144",
+   type: "image/png",
+
+  },
+  {
+   src: "/android-icon-192x192.png",
+   sizes: "192x192",
+   type: "image/png",
+
+  }
     ],
   }
 }
